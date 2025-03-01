e7f178d3192582224a7ed21d6f92b8b13052ef18
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-01 11:27:45 +0100

    esercitazione svolta

---
+++ main.js
@@ -0,0 +1,22 @@
+let num1 = 10;
+
+let num2 = 5;
+
+
+console.log( 'Il valore della variabile num1 è ' + num1 );
+
+console.log( 'il valore della variabile num2 è ' + num2 );
+
+let stringa = 'Ciao a tutti sono Francesco';
+
+console.log( 'Il valore della variabile stringa è ' + stringa);
+
+stringa = 'Mi chiamo Francesco';
+ 
+console.log( stringa);
+
+const PIGRECO = 3.14159265359;
+
+console.log( 'Il valore di PIGRECO è ' + PIGRECO );
+
+PIGRECO = 3.14;
